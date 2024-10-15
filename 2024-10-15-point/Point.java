e34f15d85289aa36c6b19420136528bf2633545f
wetoyo <theduckster1231@gmail.com>
2024-10-15 11:27:30 -0400

    feat: added Constructor

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -7,8 +7,8 @@ public class Point{
 
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
-    //COMPLETE THIS SECOND
-    //Then write test cases.
+    x = other.getX;
+    y = other.getY;
   }
 
   //Initialize this Point to have the provided values
