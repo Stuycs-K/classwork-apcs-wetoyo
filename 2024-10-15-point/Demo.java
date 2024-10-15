f73dce0f1cd2de901efc50da15af16945e4b5a1c
wetoyo <theduckster1231@gmail.com>
2024-10-15 11:40:30 -0400

    test: added equilaterial triangle test

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -17,5 +17,13 @@ public class Demo{
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+
+    Point v1 = new Point(0,0);
+    Point v2 = new Point(1,0);
+    Point v3 = new Point(0.5, Math.sqrt(3) / 2);
+
+    System.out.println(v1.distanceTo(v2));
+    System.out.println(v2.distanceTo(v3));
+    System.out.println(v3.distanceTo(v1));
   }
 }
