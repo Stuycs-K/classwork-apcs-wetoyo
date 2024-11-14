787565b08e2603fc88b582830d98583e39f5d69a
wetoyo <theduckster1231@gmail.com>
2024-11-14 11:25:04 -0500

    feat: added replace empty method

---
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+public class ArrayListPractice
+{
+  public static ArrayList<String>createRandomArray(int size)
+  {
+    ArrayList result = new ArrayList<String>();
+    for (int i = 0; i < size; i++)
+    {
+      String add = "";
+      int rand = (int)(Math.random() * 11);
+      add += rand == 0? "" : rand;
+      result.add(add);
+    }
+  }
+  public static void replaceEmpty(ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+    for (int i = 0; i < original.size(); i++)
+    {
+      original.set(i, "Empty");
+    }
+  }
+
+  public static ArrayList<String> makeReversedList(ArrayList<String> original){
+  //return a new ArrayList that is in the reversed order of the original.
+    ArrayList result = new ArrayList<String>();
+    for (int i = original.size(); i > 0; i++)
+    {
+      result.add(original.get(i));
+    }
+    return result;
+  }
+
+  public static ArrayList<String> mixLists(ArrayList<String> a, ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //If one list is longer than the other, just attach the remaining values to the end.
+    ArrayList result = new ArrayList<String>();
+    for (int i = 0; i < (a.size() > b.size()? a.size(): b.size()); i++)
+    {
+        if (i < a.size())
+        {
+          result.add(a.get(i));
+        }
+        if (i < b.size())
+        {
+          result.add(b.get(i));
+        }
+    }
+    return result;
+  }
+}
