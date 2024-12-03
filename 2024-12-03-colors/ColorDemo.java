75ae049845139b70dcad3e3e5301fba4e6ef8972
wetoyo <theduckster1231@gmail.com>
2024-12-03 11:42:48 -0500

    finished cw

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -6,7 +6,14 @@ public class ColorDemo
 
   public static void main (String[] args)
   {
-
+    for(int r = 0; r < 256; r+=3){
+      for(int g = 0; g <= 256; g+=2){
+        for(int b = 0; b <= 256; b+=1){
+          System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+        }
+      }
+        System.out.println();
+    }
   }
 
   public static void sleep(int milli)
@@ -22,8 +29,8 @@ public class ColorDemo
     System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
   }
 
-  public static Void color(int foreground, int background, int modifier)
+  public static void color(int foreground, int background, int modifier)
   {
-    System.out.print( "\u001b[" + foreground + ";" + (10+background) + + ";" + modifier + "m");
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
   }
 }
